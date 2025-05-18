6fd5a5c3f11cf240ec64cfaf4ae744d6eaec0274
Davit <davit_babayan@epam.com>
2025-05-18 22:37:26 +0400

    New product page

--- flaskr/blueprints/admin.py
+++ flaskr/blueprints/admin.py
@@ -4,7 +4,7 @@ from flask_login import login_required, current_user
 import os.path
 from time import time
 
-from flaskr.models import User
+from flaskr.models import Product
 from flaskr import db
 
 admin_bp = Blueprint('admin', __name__, url_prefix='/')
@@ -15,16 +15,32 @@ def new_product():
     if request.method == 'POST':
         product_name = request.form.get('name')
         product_price = request.form.get('price')
+        product_in_stock = int(request.form.get('in_stock'))
         product_description = request.form.get('description')
         product_image = request.files.get('image')
 
         if not product_name or not product_price or not product_description:
             flash('All fields are required!', 'danger')
             return redirect(url_for('admin.new_product'))
+        
+        if product_in_stock < 0:
+            flash('Product count is invalid', 'error')
+            return redirect(url_for('admin.new_product'))
 
         if product_image and product_image.filename != '':
             filename = str(time()).replace('.', '') + os.path.splitext(product_image.filename)[1]
             product_image.save(os.path.join('flaskr/static/uploads', filename))
+
+        new_product = Product(
+            name=product_name,
+            price=float(product_price),
+            in_stock=int(product_in_stock),
+            description=product_description,
+            image=filename if product_image else None
+        )
+
+        db.session.add(new_product)
+        db.session.commit()
 
         flash('Product created successfully!', 'success')
         return redirect(url_for('admin.new_product'))
